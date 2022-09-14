ad67dc9764df49b286986152f89b28fe9bc22cca
Falowo-Joshua <joshuafalowo20@gmail.com>
2022-09-14 12:34:04 -0700

    m

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,22 +3,26 @@
  * print_sign - Prints the sign of a number.
  * @n: The number of which the sign will be printed.
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
+	
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
+		
 		return (0);
-	}
-	else if ( n < 0);
+	
+	else
 	}
 		_putchar('-');
+		
 		return (-1);
+	
 	}
-	return (0);
 }
