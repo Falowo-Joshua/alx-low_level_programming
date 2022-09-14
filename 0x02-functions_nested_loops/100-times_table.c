95f57ee6abafddf38bfbd6ec38a44b48fafd937f
Falowo-Joshua <joshuafalowo20@gmail.com>
2022-09-14 15:36:47 -0700

    m

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,11 +2,8 @@
 
 /**
  * print_times_table - print times table up to input
- *
  * Description: Writes times table up to imput not exceeding 15
- *
  * @n: size of times table
- * 
  * Return: Nothing
  */
 
