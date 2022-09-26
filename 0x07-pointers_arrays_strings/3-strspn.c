8d12a2fb73ef9ffcd124803e0de2e225d46fbe01
Falowo-Joshua <joshuafalowo20@gmail.com>
2022-09-26 16:46:45 -0700

    m

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,13 +9,13 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
+ 	unsigned int bytes = 0;
 	int index;
 
 	while (*s)
 	{
-		 for (index = 0; accept[index]; index++)
-		 {
+		for (index = 0; accept[index]; index++)
+		{
 			if (*s == accept[index])
 			{
 				 bytes++;
@@ -24,12 +24,10 @@ unsigned int _strspn(char *s, char *accept)
 
 			else if (accept[index + 1] == '\0')
 				return (bytes);
-		 }
+		}
 
-		 s++;
+		s++;
 	}
 
 	return (bytes);
 }
-
-
