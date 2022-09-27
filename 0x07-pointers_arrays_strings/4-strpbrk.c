69acdd0ac4a549f8bfc2b19e8ae074b45d9b356f
Falowo-Joshua <joshuafalowo20@gmail.com>
2022-09-26 17:00:21 -0700

    m

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- * File: 4-strpbrk.c
  * _strpbrk - Searches a string for any of a set of bytes.
  * @s: The string to be searched.
  * @accept: The set of bytes to be searched for.
