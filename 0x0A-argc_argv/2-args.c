3a39fb1904e545f2f2a8928181c4abc9af2702ca
Falowo-Joshua <joshuafalowo20@gmail.com>
2022-09-30 06:13:41 -0700

    m

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,12 +11,15 @@
 
 int main(int argc, char *argv[])
 {
-/*Declaring variables*/                                                         int count = 0;
+	/*Declaring variables*/                                                         int count = 0;
 
-if (argc > 0)
-{
-/*WHILE - Print each arguments*/
-while (count < argc)
-{
-printf("%s\n", argv[count]);
+	if (argc > 0)
+	{
+	/*WHILE - Print each arguments*/
+	while (count < argc)
+	}
+	
+	{						
+	printf("%s\n", argv[count]);
+	}
 }
