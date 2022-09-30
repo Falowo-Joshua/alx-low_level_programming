3c53b83a0ae574eb392019b58467af51fc280d0a
Falowo-Joshua <joshuafalowo20@gmail.com>
2022-09-30 05:50:49 -0700

    m

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; /*Ignore argv*/
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
