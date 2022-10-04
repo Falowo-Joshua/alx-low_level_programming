4a3ac4339ddd828bc16f448ff67da0fef5dd1099
Falowo-Joshua <joshuafalowo20@gmail.com>
2022-10-04 09:35:40 -0700

    m

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -20,4 +20,27 @@ char *argstostr(int ac, char **av)
 	for (i = 0, total = 0; i < ac; i++)
 	{
 		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
+			;
+		total++;
+	}
+	total++;
+
+	a = malloc(total * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+
+	retp = a;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			{
+				*a = av[i][j];
+				a++;
+			}
+			*a = '\n';
+			a++;
+	}
+
+	return (retp);
+}
 
